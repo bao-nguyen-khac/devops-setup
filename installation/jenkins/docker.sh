9d7da8c310d5f1784f99235aa59b8f3cd836998b
bao-nguyen-khac <khacbao7122001@gmail.com>
2022-11-22 13:38:33 +0700

    shell install docker

--- installation/jenkins/docker.sh
+++ installation/jenkins/docker.sh
@@ -1,1 +1,1 @@
-docker run -u 0 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -v ~/jenkins:/var/jenkins_home -p 8080:8080  --name jenkins -d jenkins/jenkins:lts-jdk11
+docker run -u 0 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -v ~/jenkins_volume:/var/jenkins_home -p 8080:8080  --name jenkins -d jenkins/jenkins:lts-jdk11
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,11 @@
 sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+sudo apt-get update
+sudo apt-get install docker-compose-plugin -y
 sudo apt install ansible -y
 sudo apt install nginx -y
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
