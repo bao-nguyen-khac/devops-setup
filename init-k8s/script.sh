44b4a9b313b062f1dc04aacee0600593d968ff1c
Nguyen Khac Bao <khacbao7122001@gmail.com>
2024-02-27 09:38:34 +0700

    update k8s script

---
+++ init-k8s/script.sh
@@ -0,0 +1,10 @@
+# https://docs.google.com/spreadsheets/d/1bLh4PDURGhDNToxQTczgioASCIJqHNCRvFR_hux6tgM/edit?usp=sharing
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo swapoff -a
+sudo rm -f /etc/containerd/config.toml && sudo systemctl restart containerd && sudo systemctl enable containerd
